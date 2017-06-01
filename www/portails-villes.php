b5642be5e494e68448eaabe010b8afa4e57b6db5
Antoine Boulanger <antoine@locomotive.ca>
2017-06-01 15:41:37 -0400

    Page portails villes

---
+++ www/portails-villes.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<!--[if lte IE 9]>     <html lang="fr" class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang="fr"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="theme-color" content="#4f89e9">
+		<title>Locomotive | Portails de villes</title>
+
+		<link rel="apple-touch-icon" href="modules/memo/assets/images/apple-touch-icon.png">
+		<link rel="icon" href="modules/memo/assets/images/favicon.png">
+
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,500" rel="stylesheet" type="text/css">
+
+		<link rel="stylesheet" href="modules/memo/assets/styles/dist/main.css">
+	</head>
+	<body>
+
+		<div class="o-layout">
+			<div class="o-layout_header -loco">
+				<header class="c-header-main">
+					<a class="c-logo-main -noflex" href="/">
+						<div class="c-logo-main_wrap">
+							<span class="c-logo-main_loco u-fadeIn js-wow" data-wow-delay="0.6s">
+								<svg role="img"><use xlink:href="modules/memo/assets/images/dist/svgs.svg#locomotive"></use></svg>
+							</span>
+						</div>
+					</a>
+				</header>
+
+			<main>
+				<section class="c-home o-section -color">
+					<div class="c-home_content">
+						<h1 class="o-h1 u-fadeInUpSmall js-wow" data-wow-delay="0.8s">Des portails citoyens uniques et modernes</h1>
+						<p class="o-text u-fadeInUpSmall js-wow" data-wow-delay="1.0s">
+							Simplifiez votre communication et faites rayonner votre ville.
+						</p>
+						<a class="o-button -white -arrow u-fadeInUpSmall js-wow" href="https://locomotive.ca/" data-wow-delay="1.3s">
+							<span class="o-button_arrow">
+								<svg class="o-button_arrow_icon" role="img"><use xlink:href="modules/memo/assets/images/dist/svgs.svg#arrow-down"></use></svg>
+							</span>
+							En savoir plus
+						</a>
+					</div>
+				</section>
+			</main>
+			</div>
+			<div class="o-layout_main -loco">
+				<div class="o-block-grid o-grid">
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.5s">
+						<a class="c-block" href="http://candiac.ca/" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_3.svg" alt="Ville de Candiac">
+							</span>
+						</a>
+					</div>
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.7s">
+						<a class="c-block" href="http://www.villepincourt.qc.ca/" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_5.svg" alt="Ville de Pincourt">
+							</span>
+						</a>
+					</div>
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.8s">
+						<a class="c-block" href="http://www.pointe-claire.ca/" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_13.svg" alt="Ville de Pointe-Claire">
+							</span>
+						</a>
+					</div>
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.9s">
+						<a class="c-block" href="https://www.ville.st-augustin.qc.ca/" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_11.svg" alt="Ville de Saint-Augustin-de-Desmaures">
+							</span>
+						</a>
+					</div>
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.0s">
+						<a class="c-block" href="https://saint-constant.ca/" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_6.svg" alt="Ville de Saint-Constant">
+							</span>
+						</a>
+					</div>
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.1s">
+						<a class="c-block" href="https://www.ville.sainte-julie.qc.ca/fr/200/Accueil-Portail_web_de_la_Ville_de_Sainte-Julie" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_7.svg" alt="Ville de Sainte-Julie">
+							</span>
+						</a>
+					</div>
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.2s">
+						<a class="c-block" href="http://www.ville.terrebonne.qc.ca/" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_14.svg" alt="Ville de Terrebonne">
+							</span>
+						</a>
+					</div>
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.3s">
+						<a class="c-block" href="https://ville.valdor.qc.ca" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_15.svg" alt="Ville de Val-d'Or">
+							</span>
+						</a>
+					</div>
+					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.4s">
+						<a class="c-block" href="https://www.ville.vaudreuil-dorion.qc.ca/" target="_blank">
+							<span class="c-block_image">
+								<img src="modules/memo/assets/images/cities/logo_16.svg" alt="Ville de Vaudreuil-Dorion">
+							</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="modules/memo/assets/scripts/dist/jquery-1.11.3.min.js"><\/script>')</script>
+		<script src="modules/memo/assets/scripts/dist/vendors.js"></script>
+		<script src="modules/memo/assets/scripts/dist/app.js"></script>
+
+		<script>
+	        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	        ga('create', 'UA-1837138-68', 'auto');
+	        ga('send', 'pageview');
+        </script>
+	</body>
+</html>
