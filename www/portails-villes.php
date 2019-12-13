a5ae7d6830677c131163d5c3d8aa9dc22b9c2364
Antoine Boulanger <antoine@locomotive.ca>
2019-12-13 10:52:31 -0500

    Change pointe-claire for mirabel in portails villes

--- www/portails-villes.php
+++ www/portails-villes.php
@@ -1,132 +1,132 @@
 <!doctype html>
 <!--[if lte IE 9]>     <html lang="fr" class="ie9"> <![endif]-->
 <!--[if gt IE 9]><!--> <html lang="fr"> <!--<![endif]-->
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="theme-color" content="#4F89E9">
-		<title>Locomotive | Portails de villes</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#4F89E9">
+        <title>Locomotive | Portails de villes</title>
 
-		<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
-		<link rel="icon" href="assets/images/favicon.png">
+        <link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+        <link rel="icon" href="assets/images/favicon.png">
 
-		<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,500" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,500" rel="stylesheet" type="text/css">
 
-		<link rel="stylesheet" href="assets/styles/main.css">
-	</head>
-	<body>
+        <link rel="stylesheet" href="assets/styles/main.css">
+    </head>
+    <body>
 
-		<div class="o-layout">
-			<div class="o-layout_header -loco">
-				<header class="c-header-main">
-					<a class="c-logo-main -noflex" href="https://locomotive.ca/">
-						<div class="c-logo-main_wrap">
-							<span class="c-logo-main_loco u-fadeIn js-wow" data-wow-delay="0.6s">
-								<svg role="img"><use xlink:href="assets/images/sprite.svg#locomotive"></use></svg>
-							</span>
-						</div>
-					</a>
-				</header>
+        <div class="o-layout">
+            <div class="o-layout_header -loco">
+                <header class="c-header-main">
+                    <a class="c-logo-main -noflex" href="https://locomotive.ca/">
+                        <div class="c-logo-main_wrap">
+                            <span class="c-logo-main_loco u-fadeIn js-wow" data-wow-delay="0.6s">
+                                <svg role="img"><use xlink:href="assets/images/sprite.svg#locomotive"></use></svg>
+                            </span>
+                        </div>
+                    </a>
+                </header>
 
-			<main>
-				<section class="c-home o-section -color -side">
-					<div class="c-home_content">
-						<h1 class="o-h1 u-fadeInUpSmall js-wow" data-wow-delay="0.8s">Des portails citoyens uniques et modernes</h1>
-						<p class="o-text u-fadeInUpSmall js-wow" data-wow-delay="1.0s">
-							Simplifiez votre communication <br>et faites rayonner votre ville.
-						</p>
-						<a class="o-button -white -arrow u-fadeInUpSmall js-wow" href="https://locomotive.ca/" data-wow-delay="1.3s">
-							<span class="o-button_arrow">
-								<svg class="o-button_arrow_icon" role="img"><use xlink:href="assets/images/sprite.svg#arrow-down"></use></svg>
-							</span>
-							En savoir plus
-						</a>
-					</div>
-				</section>
-			</main>
-			</div>
-			<div class="o-layout_main -loco">
-				<div class="o-block-grid o-grid">
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.5s">
-						<a class="c-block" href="http://candiac.ca/" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_3.svg" alt="Ville de Candiac">
-							</span>
-						</a>
-					</div>
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.7s">
-						<a class="c-block" href="http://www.villepincourt.qc.ca/" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_18.svg" alt="Ville de Pincourt">
-							</span>
-						</a>
-					</div>
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.8s">
-						<a class="c-block" href="http://www.pointe-claire.ca/" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_13.svg" alt="Ville de Pointe-Claire">
-							</span>
-						</a>
-					</div>
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.9s">
-						<a class="c-block" href="https://www.ville.st-augustin.qc.ca/" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_11.svg" alt="Ville de Saint-Augustin-de-Desmaures">
-							</span>
-						</a>
-					</div>
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.0s">
-						<a class="c-block" href="https://saint-constant.ca/" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_6.svg" alt="Ville de Saint-Constant">
-							</span>
-						</a>
-					</div>
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.1s">
-						<a class="c-block" href="https://www.villerdl.ca/fr" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_17.svg" alt="Ville de Rivière-du-Loup">
-							</span>
-						</a>
-					</div>
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.2s">
-						<a class="c-block" href="http://www.ville.terrebonne.qc.ca/" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_14.svg" alt="Ville de Terrebonne">
-							</span>
-						</a>
-					</div>
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.3s">
-						<a class="c-block" href="https://ville.valdor.qc.ca" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_15.svg" alt="Ville de Val-d'Or">
-							</span>
-						</a>
-					</div>
-					<div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.4s">
-						<a class="c-block" href="https://www.ville.vaudreuil-dorion.qc.ca/" target="_blank">
-							<span class="c-block_image">
-								<img src="assets/images/cities/logo_16.svg" alt="Ville de Vaudreuil-Dorion">
-							</span>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
+            <main>
+                <section class="c-home o-section -color -side">
+                    <div class="c-home_content">
+                        <h1 class="o-h1 u-fadeInUpSmall js-wow" data-wow-delay="0.8s">Des portails citoyens uniques et modernes</h1>
+                        <p class="o-text u-fadeInUpSmall js-wow" data-wow-delay="1.0s">
+                            Simplifiez votre communication <br>et faites rayonner votre ville.
+                        </p>
+                        <a class="o-button -white -arrow u-fadeInUpSmall js-wow" href="https://locomotive.ca/" data-wow-delay="1.3s">
+                            <span class="o-button_arrow">
+                                <svg class="o-button_arrow_icon" role="img"><use xlink:href="assets/images/sprite.svg#arrow-down"></use></svg>
+                            </span>
+                            En savoir plus
+                        </a>
+                    </div>
+                </section>
+            </main>
+            </div>
+            <div class="o-layout_main -loco">
+                <div class="o-block-grid o-grid">
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.5s">
+                        <a class="c-block" href="http://candiac.ca/" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_3.svg" alt="Ville de Candiac">
+                            </span>
+                        </a>
+                    </div>
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.7s">
+                        <a class="c-block" href="http://www.villepincourt.qc.ca/" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_18.svg" alt="Ville de Pincourt">
+                            </span>
+                        </a>
+                    </div>
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.8s">
+                        <a class="c-block" href="https://mirabel.ca/" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_19.svg" alt="Ville de Mirabel">
+                            </span>
+                        </a>
+                    </div>
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="1.9s">
+                        <a class="c-block" href="https://www.ville.st-augustin.qc.ca/" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_11.svg" alt="Ville de Saint-Augustin-de-Desmaures">
+                            </span>
+                        </a>
+                    </div>
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.0s">
+                        <a class="c-block" href="https://saint-constant.ca/" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_6.svg" alt="Ville de Saint-Constant">
+                            </span>
+                        </a>
+                    </div>
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.1s">
+                        <a class="c-block" href="https://www.villerdl.ca/fr" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_17.svg" alt="Ville de Rivière-du-Loup">
+                            </span>
+                        </a>
+                    </div>
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.2s">
+                        <a class="c-block" href="http://www.ville.terrebonne.qc.ca/" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_14.svg" alt="Ville de Terrebonne">
+                            </span>
+                        </a>
+                    </div>
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.3s">
+                        <a class="c-block" href="https://ville.valdor.qc.ca" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_15.svg" alt="Ville de Val-d'Or">
+                            </span>
+                        </a>
+                    </div>
+                    <div class="o-block-grid_item u-fadeInUpSmall js-wow" data-wow-delay="2.4s">
+                        <a class="c-block" href="https://www.ville.vaudreuil-dorion.qc.ca/" target="_blank">
+                            <span class="c-block_image">
+                                <img src="assets/images/cities/logo_16.svg" alt="Ville de Vaudreuil-Dorion">
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script>window.jQuery || document.write('<script src="assets/scripts/jquery-1.11.3.min.js"><\/script>')</script>
-		<script src="assets/scripts/vendors.js"></script>
-		<script src="assets/scripts/app.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="assets/scripts/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="assets/scripts/vendors.js"></script>
+        <script src="assets/scripts/app.js"></script>
 
-		<script>
-	        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	        ga('create', 'UA-1837138-68', 'auto');
-	        ga('send', 'pageview');
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-1837138-68', 'auto');
+            ga('send', 'pageview');
         </script>
-	</body>
+    </body>
 </html>
